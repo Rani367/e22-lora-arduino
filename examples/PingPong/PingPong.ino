c4d695af62d6e37c6a69ff934f9668bd5dd09e59
Rani367 <rani2011367@gmail.com>
2026-09-06 07:30:58 +0300

    same for PingPong

--- examples/PingPong/PingPong.ino
+++ examples/PingPong/PingPong.ino
@@ -9,51 +9,60 @@
 
 #define ROLE_SENDER 1
 
-constexpr int8_t PIN_M0 = 32;
-constexpr int8_t PIN_M1 = 33;
-constexpr int8_t PIN_AUX = 34;
-constexpr int8_t PIN_RESET = 27;
-constexpr int8_t PIN_RX = 16;
-constexpr int8_t PIN_TX = 17;
+#if defined(ESP32)
+#define RADIO_SERIAL Serial2
+constexpr int8_t PIN_M0 = 32, PIN_M1 = 33, PIN_AUX = 34, PIN_RESET = 27;
+constexpr int8_t PIN_RX = 16, PIN_TX = 17;
+#elif defined(HAVE_HWSERIAL1)
+#define RADIO_SERIAL Serial1  // ATmega328PB USART1: PB4 = RXD1, PB3 = TXD1
+constexpr int8_t PIN_M0 = 4, PIN_M1 = 5, PIN_AUX = 6, PIN_RESET = -1;
+constexpr int8_t PIN_RX = -1, PIN_TX = -1;
+#else
+#define RADIO_SERIAL Serial
+constexpr int8_t PIN_M0 = 4, PIN_M1 = 5, PIN_AUX = 6, PIN_RESET = -1;
+constexpr int8_t PIN_RX = -1, PIN_TX = -1;
+#endif
+#define DEBUG Serial
 
-E22 radio(Serial2, PIN_M0, PIN_M1, PIN_AUX, PIN_RESET);
+E22 radio(RADIO_SERIAL, PIN_M0, PIN_M1, PIN_AUX, PIN_RESET);
 
 uint32_t counter = 0;
 uint32_t lastSend = 0;
 
-char line[128];
-size_t lineLen = 0;
+char line[64];
+uint8_t lineLen = 0;
 bool awaitingRssi = false;
 
 void setup() {
-  Serial.begin(115200);
+  DEBUG.begin(115200);
   delay(500);
-  Serial.println(ROLE_SENDER ? "\nE22 PingPong: sender" : "\nE22 PingPong: responder");
+  DEBUG.println(ROLE_SENDER ? F("\nE22 PingPong: sender") : F("\nE22 PingPong: responder"));
 
-  if (!radio.begin(9600, PIN_RX, PIN_TX)) Serial.println("begin() failed");
+  if (!radio.begin(9600, PIN_RX, PIN_TX)) DEBUG.println(F("begin() failed"));
 
   // Same channel and address on both ends, RSSI byte on.
   E22Config cfg;
   if (!radio.readConfig(cfg)) {
-    Serial.println("readConfig() failed");
+    DEBUG.println(F("readConfig() failed"));
     return;
   }
-  cfg.channel = 23;          // 433.125 MHz
+  cfg.channel = 23;          // 433.125 MHz, away from the 436.4 MHz main radio
   cfg.address = 0x0000;
   cfg.netId = 0;
   cfg.airRate = E22AirRate::Rate2k4;
   cfg.rssiByte = true;
-  if (!radio.writeConfig(cfg)) Serial.println("writeConfig() failed");
+  if (!radio.writeConfig(cfg)) DEBUG.println(F("writeConfig() failed"));
 }
 
 void handleLine(const char* text, int rssiDbm) {
-  Serial.printf("rx  \"%s\"  rssi %d dBm\n", text, rssiDbm);
+  DEBUG.print(F("rx  \"")); DEBUG.print(text);
+  DEBUG.print(F("\"  rssi ")); DEBUG.print(rssiDbm); DEBUG.println(F(" dBm"));
 #if !ROLE_SENDER
   if (strncmp(text, "PING ", 5) == 0) {
-    char reply[32];
+    char reply[24];
     snprintf(reply, sizeof reply, "PONG %s\n", text + 5);
     radio.send(reply);
-    Serial.printf("tx  \"PONG %s\"\n", text + 5);
+    DEBUG.print(F("tx  \"PONG ")); DEBUG.print(text + 5); DEBUG.println('"');
   }
 #endif
 }
@@ -81,10 +90,10 @@ void loop() {
 #if ROLE_SENDER
   if (millis() - lastSend >= 2000) {
     lastSend = millis();
-    char msg[32];
+    char msg[24];
     snprintf(msg, sizeof msg, "PING %lu\n", (unsigned long)counter++);
     radio.send(msg);
-    Serial.printf("tx  \"PING %lu\"\n", (unsigned long)(counter - 1));
+    DEBUG.print(F("tx  \"PING ")); DEBUG.print(counter - 1); DEBUG.println('"');
   }
 #endif
 }
